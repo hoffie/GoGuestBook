7c7d1e044299e93e0808fb48209b8511d67ccb0b
Christian Hoffmann <christian@hoffie.info>
2025-09-14 01:25:05 +0200

    Initial commit

---
+++ example.sh
@@ -0,0 +1,11 @@
+export GGB_ANTI_SPAM_CODE=1992
+export GGB_ENTRY_WAIT_SECONDS=3
+export GGB_URL=http://localhost:8080/static/demo
+export GGB_SMTP_HOST=localhost
+export GGB_SMTP_PORT=25
+export GGB_SMTP_USER=
+export GGB_SMTP_PASS=
+export GGB_ADMIN_EMAIL=foo@example.org
+export GGB_LISTEN=127.0.0.1:8080
+export GGB_DB_FILE=guestbook.db
+./GoGuestBook
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/hoffie/GoGuestBook
+
+go 1.24.6
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/mattn/go-sqlite3 v1.14.32
+)
